20a7a2dec40d327808bf9e37362290cc2ab5cf8e
BuffTaco <jiayue.quan@gmail.com>
2023-09-25 21:10:53 -0700

    set up page changing

--- foodMeasure/src/App.jsx
+++ foodMeasure/src/App.jsx
@@ -1,23 +1,74 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const calc = () => {
   return (
     <>
-      <h1>Food tracker</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      
+      <h1>placeholder</h1>
+    
     </>
   )
+}
+const about = () => {
+  return (
+    <>
+        <h1>Food tracker</h1>
+      <div className="card">
+        <a href="">Test</a>
+      </div>
+
+    </>
+    )
+}
+const compare = () => {
+  return (
+    <>
+    <h1>comparing</h1>
+    </>
+  )
+}
+const rec = () => {
+  return (
+    <>
+    <h1>Food recs</h1>
+    </>
+  )
+}
+function App() {
+  
+  const [show, setShow] = useState("about")
+  
+    return (
+      <>
+        <nav className="banner">
+        <ul>
+          <button onClick={() => setShow("about")}>About</button>
+          <button onClick={() => setShow("calc")}>Nutrition Calculator</button>
+          <button onClick={() => setShow("compare")}>Compare</button>
+          <button onClick={() => setShow("rec")}>Recc Food</button>
+        </ul></nav>
+        <h1>save me</h1>
+        {
+          (show === "calc") ?
+          calc()
+          :
+          (show === "about") ?
+          about()
+          :
+          (show === "compare") ?
+          compare()
+          :
+          (show === "rec") ?
+          rec()
+          :
+          about()
+        }
+      
+      </>
+      
+    )
+    
+  
 }
 
 export default App
